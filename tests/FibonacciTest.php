a85a743f0d494d6ef516e602bc1e2d5e09ba435f
Pedro Morales <pedromorales@grupojuinsa.es>
2020-02-10 12:42:11 +0100

    RED: check the 6 firsts numbers with an array

--- tests/FibonacciTest.php
+++ tests/FibonacciTest.php
@@ -33,11 +33,23 @@ class FibonacciTest extends TestCase
     }
 
     /** @test */
-    public function if_index_is_trhee_return_value_one()
+    public function if_index_is_three_return_value_one()
     {
         $value = $this->fibonacci->getValueByIndex(3);
 
         $this->assertEquals(1, $value, sprintf("Result for index 1 should be 0 and got %s", $value));
+    }
+
+    /** @test */
+    public function check_first_six_numbers_with_array_of_values_and_returns()
+    {
+        $pairs = array("1" => 0, "2" => 1, "3" => 1, "4" => 2, "5" => 3, "6" => 5);
+
+        foreach ($pairs as $index => $value) {
+            $calculatedValue = $this->fibonacci->getNextItemNumber((int)$index);
+            $this->assertEquals($value, $calculatedValue, sprintf("For index %s, calculated value % does not match passed %s", $index, $value, $calculatedValue));
+        }
+
     }
 
 }
